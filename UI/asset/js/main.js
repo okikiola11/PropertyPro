3787431a280e4154aa4554419cff4b3515ebd1ee
Apelehin Okikiola <okikiola.apelehin@gmail.com>
2019-06-17 17:18:33 +0100

    ft(mark property): user agent should be to mark his/her posted advert as sold create a mark posted property advert as sold option
    [Delivers #166677583]

--- UI/asset/js/main.js
+++ UI/asset/js/main.js
@@ -1,6 +1,28 @@
 const navTrigger = document.querySelector('.nav-trigger');
 const sideNav = document.querySelector('.side-nav');
+const soldDiv = document.querySelector('.main-content__sold');
+const markDiv = document.querySelector('.list-icons__sp:nth-of-type(2)')
+//const hideShow = document.querySelector('.main-content__cc');
+const load = document.querySelectorAll('.main-content__lintin-wrapper');
 
 navTrigger.addEventListener('click', () => {
     sideNav.classList.toggle('visible');
 });
+
+// load.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     alert('hello');
+//     console.log('hello again');
+
+//     const hide = document.querySelector('.main-content__sold span');
+//     hide.classList.add(hide);
+
+//     //soldDiv.classList.add(hide);
+// })
+
+markDiv.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('hello');
+
+    soldDiv.classList.remove('hide');
+})
