a989e39384348d93ee3e85215f59c6b99ed6a941
Apelehin Okikiola <okikiola.apelehin@gmail.com>
2019-07-10 09:55:26 +0100

    ft(signin user endpoint): Setup signin user API Endpoint create a signnin user endpoint
    [Finishes #166961492]

--- API/src/controller/userController.js
+++ API/src/controller/userController.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import helper from '../utils/helper';
 import User from '../utils/userData';
 import authMiddleware from '../middleware/authMiddleware';
@@ -43,10 +44,46 @@ class UserController {
         }
       });
     } catch (error) {
-      console.log(error.stack);
       return res.status(500).json({
         status: '500 Server internal error',
         error: 'Something went wrong while trying to create a user'
+      });
+    }
+  }
+
+  static async signinUser(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = User.find(member => member.email === email);
+      if (!user) {
+        return res.status('401 Unauthorized').send('No user found.');
+      }
+      const passwordIsValid = await bcrypt.compare(password, user.password);
+      if (!passwordIsValid) {
+        return res.status(401).json({
+          status: 'Unauthorized',
+          message: 'Incorrect Password'
+        });
+      }
+      const { id, is_admin, first_name, last_name } = user;
+
+      const token = authMiddleware.generateToken({ id, is_admin });
+      return res.status(200).json({
+        status: 'success',
+        message: `Welcome ${user.email}, you have successfully logged in`,
+        data: {
+          token,
+          id,
+          first_name,
+          last_name,
+          email,
+          is_admin
+        }
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: 'Server internal error',
+        message: 'Something went wrong while trying to process your request'
       });
     }
   }
